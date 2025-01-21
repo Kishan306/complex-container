74e92dd06f3f9778f41e40d363ab6c256ca7c338
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2025-01-21 12:33:42 +0530

    dummy build commit

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn ReactJS
+            Learn ReactJS, quick!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
